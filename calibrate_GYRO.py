ec0a69d22e8558a352b063042ef0c8327b705a23
SSTrontinum <sstrontium1@gmail.com>
2025-03-29 15:41:18 +0800

    Calibrate gyro

--- calibrate_GYRO.py
+++ calibrate_GYRO.py
@@ -1,17 +1,6 @@
-import GY271
-from time import sleep
+import qmc5883l
 
-sensor = GY271.compass()
+compass = qmc5883l.QMC5883L()
 
-print('[Press CTRL + C to end the script!]')
-try:
-    while True:
-        angle = sensor.get_bearing()       
-        temp = sensor.read_temp()
-    
-        print('Heading Angle = {} deg'.format(angle))
-        print('Temperature = {:1f} C'.format(temp))
-        sleep(2)
-
-except KeyboardInterrupt:
-    print('\nScript end!')
+x, y, z = compass.get_magnet()
+print(f"X: {x}, Y: {y}, Z: {z}")
