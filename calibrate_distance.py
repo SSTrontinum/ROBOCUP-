ac5f73848b40588b39a42379dc954d3b57d547e6
SSTrontinum <sstrontium1@gmail.com>
2025-03-30 14:50:33 +0800

    QMC5883L calibration

--- calibrate_distance.py
+++ calibrate_distance.py
@@ -2,8 +2,9 @@ import serial, time
 ser = serial.Serial('/dev/serial0', 9600, timeout=1)
 ser.reset_input_buffer()
 ser.flush()
-ser.write(b"510,510\n")
-time.sleep(1)
+ser.write(b"220,290\n")
+#time.sleep(1)
+input()
 ser.write(b"255,255\n")
 """
 input()
